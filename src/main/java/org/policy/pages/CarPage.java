94349867e41a57288541b2a183d15e285c43e045
jitabhsin <jitabhsin@gmail.com>
2026-06-30 14:19:09 +0530

    constructor

--- src/main/java/org/policy/pages/CarPage.java
+++ src/main/java/org/policy/pages/CarPage.java
@@ -1,4 +1,17 @@
 package org.policy.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.policy.utils.WaitUtils;
+
 public class CarPage {
+
+    WebDriver driver;
+    WaitUtils waitUtils;
+
+    public CarPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
 }
