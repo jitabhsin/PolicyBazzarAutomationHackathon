ce8413d755abad34037bd952b5d201b939f22c1a
V R <2497728@cognizant.com>
2026-07-01 11:05:35 +0530

    Updated

--- src/test/java/test/TC_13SelectCity.java
+++ src/test/java/test/TC_13SelectCity.java
@@ -10,17 +10,14 @@ public class TC_13SelectCity extends BaseTest {
 
     HomePage homePage;
     CarPage carPage;
-    CarSelectPage carSelectPage;
 
     @Test
     public void selectCity() {
         homePage = new HomePage(driver);
         carPage = new CarPage(driver);
-        carSelectPage = new CarSelectPage(driver);
 
         homePage.clickCarInsurance();
         carPage.clickNewCar();
-        carSelectPage.selectCityElement();
 
 
 
